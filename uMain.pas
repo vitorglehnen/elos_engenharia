89c21408a5bbf793bd5e871f4ac2ac40c8212630
Vitor Gabriel Lehnen <107063776+vitorglehnen@users.noreply.github.com>
2024-05-03 15:44:43 -0300

    first commit

---
+++ uMain.pas
@@ -0,0 +1,53 @@
+unit uMain;
+
+interface
+
+uses
+  Winapi.Windows,
+  Winapi.Messages,
+  System.SysUtils,
+  System.Variants,
+  System.Classes,
+  Vcl.Graphics,
+  Vcl.Controls,
+  Vcl.Forms,
+  Vcl.Dialogs,
+  Vcl.ExtCtrls,
+  Vcl.Menus,
+  formClientes,
+  Vcl.Imaging.pngimage;
+
+type
+  TfrmMain = class(TForm)
+    pnlPrincipal: TPanel;
+    pnlForms: TPanel;
+    mnPrincipal: TMainMenu;
+    Cadastros1: TMenuItem;
+    Parmetros1: TMenuItem;
+    Sair1: TMenuItem;
+    Clientes1: TMenuItem;
+    Projetos1: TMenuItem;
+    Funcionrios1: TMenuItem;
+    imgLogo: TImage;
+    procedure Clientes1Click(Sender: TObject);
+  private
+    { Private declarations }
+
+    frmClientes : TfrmClientes;
+  public
+    { Public declarations }
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+{$R *.dfm}
+
+procedure TfrmMain.Clientes1Click(Sender: TObject);
+begin
+  frmClientes := TFrmClientes.Create(nil);
+end;
+
+end.
